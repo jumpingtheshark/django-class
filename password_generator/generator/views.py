bf14e688674be12fc80e8f541e829433b541df96
jumpingtheshark <mike.rubinstein@gmail.com>
2022-02-22 17:43:41 -0600

    blah

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse #added
 
 # Create your views here.
 
-def home (request):
+def home (request): #function added
     return  HttpResponse("fuck you")
